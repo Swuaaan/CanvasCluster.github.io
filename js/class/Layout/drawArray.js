463a7f1e1cd02b6c1fd218484e97549943366693
Swuaaan <kiantetzlaff@gmail.com>
2023-12-06 22:23:53 +0100

    exportet drawArray in to an module

--- js/class/Layout/drawArray.js
+++ js/class/Layout/drawArray.js
@@ -1,3 +1,8 @@
+import polygon from '../shape/polygon.js';
+import canvas from '../controller/canvas.js';
+import icon from '../shape/icon.js';
+import settings from '../../../settings.json' assert {type: 'json'};
+
 /**
  * DrawArray.js
  * 
@@ -16,50 +21,113 @@
 // import icon from './shape/icon.js';
 // import drawArray from './Layout/drawArray.js';
 
+// Beispiel: Erstelle ein Canvas-Manager und ein Polygon mit 6 Ecken und einem Copyright-Symbol
+const	controller		= new canvas('myCanvas', settings.backcolore);
+
+const   positionArray = [],     // Array zum Speichern der x- und y-Koordinaten jeder Position
+		clustermid = [];        // Array zum Speichern der x- und y-Koordinaten jeder Position
 
 /* ================================================== */
 /* 2. Draw Array */
 /* -------------------------------------------------- */
-export default class DrawArray {
+export default class Array {
+
+    constructor(
+		tiltAngle, 
+        rowSpace, 
+        colSpace
+	) {
+		// #==== Variables ====#
+		this.tiltAngle		= tiltAngle;
+		this.rowSpace		= rowSpace;
+		this.colSpace		= colSpace;
+
+		// #==== Functions ====#
+        this.createArray();
+        this.drawArray();
+		
+	}
+
 	// #=#=#=#=#=#  #=#=#=#=#=#
-	
-
-	// #=#=#=#=#=# 2.2 Constructor #=#=#=#=#=#
-    constructor(iconClass, polygonClass) {
-        this.iconClass = iconClass;
-        this.polygonClass = polygonClass;
-        this.iconArray = [];
-        this.xSpacing = 50;
-        this.ySpacing = 50;
-    }
-
-
-	// #=#=#=#=#=# 2.3 Set horizontal Space #=#=#=#=#=#
-    setXSpacing(xSpacing) {
-        this.xSpacing = xSpacing;
-    }
-
-
-	// #=#=#=#=#=# 2.4 Set vrtical Space #=#=#=#=#=#
-    setYSpacing(ySpacing) {
-        this.ySpacing = ySpacing;
-    }
-
-
-	// #=#=#=#=#=# 2.5 Draw #=#=#=#=#=#
-	/**
-	 * Draws a grid of Polygons
-	 * @param {Number} rows 		Row Count
-	 * @param {Number} columns 		Column Count
-	 */
-    drawArray(rows, columns) {
-        for (let row = 0; row < rows; row++) {
-            for (let col = 0; col < columns; col++) {
-                const x = col * this.xSpacing;
-                const y = row * this.ySpacing + (col % 2 === 0 ? this.ySpacing / 2 : 0);
+	createArray () {
+        let columeSpace = ((this.colSpace + 5) / 2) * canvas.factor,
+            rowSpaceing = this.rowSpace * canvas.factor;
+    
+        const	rows = Math.floor(((window.innerHeight / 100) * 125) / rowSpaceing),
+                columns = Math.floor(((window.innerWidth / 100) * 125) / columeSpace);
+    
+        for (let row = -100; row <= rows; row++) {
+            for (let col = -100; col <= columns; col++) {
+                const angle = this.tiltAngle * (Math.PI / 180);
+                const x = col * columeSpace + (row * columeSpace);
+                const y = row * rowSpaceing;
+    
+                const polygonX = x + Math.cos(angle) * columeSpace * col;
+                const polygonY = y + Math.sin(angle) * columeSpace * col;
+    
+                positionArray.push({
+                    x: polygonX,
+                    y: polygonY
+                })
+    
+                const cluster = 0.019; // Zum Beispiel 20% Chance, dass kein Fill stattfindet
+    
+                if (Math.random() < cluster) {
+                    clustermid.push({
+                        x: polygonX,
+                        y: polygonY,
+                        generat: false
+                    })
+                }
             }
         }
     }
 
+    drawArray () {
+
+        while(clustermid.length > 0) {
+            let cluster = clustermid.shift();
     
+            for(const index in positionArray) {
+                const item = positionArray[index];
+        
+                // Berechnung des Abstands vom angegebenen Punkt
+                const distance = Math.sqrt((item.x - cluster.x) ** 2 + (item.y - cluster.y) ** 2);
+    
+                // Berechnung des Fade-Offsets, indem fadeDistance vom Abstand subtrahiert wird
+                const fadeOffset = Math.max(0, distance - settings.fadeoffset);
+    
+                // Setzen der Transparenz basierend auf dem Abstand und Fade-Offset
+                let alpha = 1 - fadeOffset / settings.fadeSize; // Ändere den Divisor nach Bedarf
+    
+                // Begrenze den Alpha-Wert im Bereich [0, 1]
+                alpha = Math.max(0, Math.min(1, alpha));
+        
+                let randomSymbol = settings.symbols[Math.floor(Math.random() * settings.symbols.length)];
+                
+                if ((distance < settings.clusterSize || distance < settings.fadeSize) && (item.generat !== true)) {
+                    if (Math.random() > settings.noFillChanceColore) {
+                        if (Math.random() > settings.noFillChanceGra) {
+                            let fillColor = `rgba(78, 78, 78, ${alpha})`; // Blau mit variabler Transparenz
+                            let symbolIcon = new icon(randomSymbol, 0, `hsla(0, 0.00%, 23.10%, ${alpha})`, 20);
+                            
+                            new polygon(controller, symbolIcon, 6, item.x, item.y, 20, fillColor, 29);
+                        }
+                    }
+            
+                    else {
+                        if (distance < settings.clusterSize) {
+                            let fillColor = settings.colors[Math.floor(Math.random() * settings.colors.length)];
+                            let symbolIcon = new icon(randomSymbol, 0, settings.backcolore, 20);     
+                            
+                            new polygon(controller, symbolIcon, 6, item.x, item.y, 20, fillColor, 29); 
+                        }
+                    } 
+    
+                    item.generat = true;
+    
+                }
+            }
+        }
+    }    
 }
--- js/main.js
+++ js/main.js
@@ -10,48 +10,13 @@
 /* ================================================== */
 /* 1. Imports */
 /* -------------------------------------------------- */
-import polygon from '../js/class/shape/polygon.js';
-import canvas from '../js/class/controller/canvas.js';
-import icon from '../js/class/shape/icon.js';
 import settings from '../settings.json' assert {type: 'json'};
+import Array from './class/Layout/drawArray.js';
 
-// Beispiel: Erstelle ein Canvas-Manager und ein Polygon mit 6 Ecken und einem Copyright-Symbol
-const	controller		= new canvas('myCanvas', 'hsl(0, 0.00%, 23.10%)');
+new Array(settings.rotation, settings.rowspaching, settings.columspaching);
 
-
-/* ================================================== */
-/* 2. Colors */
-/* -------------------------------------------------- */
-const monokaiColors = [
-	'hsl(39, 70%, 55%)', // Monokai Yellow
-	'hsl(198, 100%, 47%)', // Monokai Blue
-	'hsl(154, 56%, 52%)', // Monokai Green
-	'hsl(345, 100%, 50%)', // Monokai Pink
-	'hsl(14, 100%, 58%)', // Monokai Orange
-	'hsl(204, 86%, 53%)', // Monokai Cyan
-	'hsl(16, 72%, 51%)', // Monokai Red
-	'hsl(209, 56%, 63%)', // Monokai Purple
-];
-
-
-/* ================================================== */
-/* 3. Symbols */
-/* -------------------------------------------------- */
-const fontAwesomeSymbols = [
-	'\uf2b9',
-	'\uf1f8',
-	'\uf0c8',
-	'\uf17d',
-	'\uf06e',
-	'\uf135',
-	'\uf219',
-	'\uf2dc',
-	'\uf11b',
-	'\uf1c0',
-  ];
-
-const   positionArray = [],     // Array zum Speichern der x- und y-Koordinaten jeder Position
-		clustermid = [];        // Array zum Speichern der x- und y-Koordinaten jeder Position
+// const   positionArray = [],     // Array zum Speichern der x- und y-Koordinaten jeder Position
+// 		clustermid = [];        // Array zum Speichern der x- und y-Koordinaten jeder Position
 
 // Funktion, die aufgerufen wird, wenn die Fenstergröße geändert wird
 function handleResize() {
@@ -65,89 +30,3 @@ function handleResize() {
 // Ereignislistener für das "resize"-Ereignis hinzufügen
 window.addEventListener('resize', handleResize);
 
-createArray(settings.rotation, settings.rowspaching, settings.columspaching ); 
-drawArray();
-
-function createArray (tiltAngle, rowSpace, colSpace) {
-	let columeSpace = ((colSpace + 5) / 2) * canvas.factor,
-		rowSpaceing = rowSpace * canvas.factor;
-
-	const	rows = Math.floor(((window.innerHeight / 100) * 125) / rowSpaceing),
-			columns = Math.floor(((window.innerWidth / 100) * 125) / columeSpace);
-
-	for (let row = -100; row <= rows; row++) {
-		for (let col = -100; col <= columns; col++) {
-			const angle = tiltAngle * (Math.PI / 180);
-			const x = col * columeSpace + (row * columeSpace);
-			const y = row * rowSpaceing;
-
-			const polygonX = x + Math.cos(angle) * columeSpace * col;
-			const polygonY = y + Math.sin(angle) * columeSpace * col;
-
-			positionArray.push({
-				x: polygonX,
-				y: polygonY
-			})
-
-			const cluster = 0.019; // Zum Beispiel 20% Chance, dass kein Fill stattfindet
-
-			if (Math.random() < cluster) {
-				clustermid.push({
-					x: polygonX,
-					y: polygonY,
-					generat: false
-				})
-			}
-		}
-	}
-}
-
-
-
-function drawArray () {
-
-	while(clustermid.length > 0) {
-		let cluster = clustermid.shift();
-
-		for(const index in positionArray) {
-			const item = positionArray[index];
-	
-			// Berechnung des Abstands vom angegebenen Punkt
-			const distance = Math.sqrt((item.x - cluster.x) ** 2 + (item.y - cluster.y) ** 2);
-
-			// Berechnung des Fade-Offsets, indem fadeDistance vom Abstand subtrahiert wird
-			const fadeOffset = Math.max(0, distance - settings.fadeoffset);
-
-			// Setzen der Transparenz basierend auf dem Abstand und Fade-Offset
-			let alpha = 1 - fadeOffset / settings.fadeSize; // Ändere den Divisor nach Bedarf
-
-			// Begrenze den Alpha-Wert im Bereich [0, 1]
-			alpha = Math.max(0, Math.min(1, alpha));
-	
-			let randomSymbol = settings.symbols[Math.floor(Math.random() * settings.symbols.length)];
-			
-			if ((distance < settings.clusterSize || distance < settings.fadeSize) && (item.generat !== true)) {
-				if (Math.random() > settings.noFillChanceColore) {
-					if (Math.random() > settings.noFillChanceGra) {
-						let fillColor = `rgba(78, 78, 78, ${alpha})`; // Blau mit variabler Transparenz
-						let symbolIcon = new icon(randomSymbol, 0, `hsla(0, 0.00%, 23.10%, ${alpha})`, 20);
-						
-						new polygon(controller, symbolIcon, 6, item.x, item.y, 20, fillColor, 29);
-					}
-				}
-		
-				else {
-					if (distance < settings.clusterSize) {
-						let fillColor = settings.colors[Math.floor(Math.random() * settings.colors.length)];
-						let symbolIcon = new icon(randomSymbol, 0, `hsl(0, 0.00%, 23.10%)`, 20);     
-						
-						new polygon(controller, symbolIcon, 6, item.x, item.y, 20, fillColor, 29); 
-					}
-				} 
-
-				item.generat = true;
-
-			}
-		}
-	}
-}
